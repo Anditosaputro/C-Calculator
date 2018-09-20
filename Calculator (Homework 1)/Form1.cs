f10fa798775cf280bcb73428e5b3d7d809e1aa87
Anditosaputro <43364359+Anditosaputro@users.noreply.github.com>
2018-09-20 13:30:00 +0800

    Add files via upload

---
+++ Calculator (Homework 1)/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator__Homework_1_
+{
+    public partial class Form1 : Form
+    {
+        Double resultValue = 0;
+        String operationPerformed = "";
+        bool isOperationPerformed = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void disable()
+        {
+            textBox1.Enabled = false;
+            button1.Show();
+            button2.Hide();
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = false;
+            button6.Enabled = false;
+            button7.Enabled = false;
+            button8.Enabled = false;
+            button9.Enabled = false;
+            button10.Enabled = false;
+            button11.Enabled = false;
+            button12.Enabled = false;
+            button13.Enabled = false;
+            button14.Enabled = false;
+            button15.Enabled = false;
+            button16.Enabled = false;
+            button17.Enabled = false;
+            button18.Enabled = false;
+            button19.Enabled = false;
+            button20.Enabled = false;
+        }
+
+        public void enable()
+        {
+            textBox1.Enabled = true;
+            button1.Hide();
+            button2.Show();
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button5.Enabled = true;
+            button6.Enabled = true;
+            button7.Enabled = true;
+            button8.Enabled = true;
+            button9.Enabled = true;
+            button10.Enabled = true;
+            button11.Enabled = true;
+            button12.Enabled = true;
+            button13.Enabled = true;
+            button14.Enabled = true;
+            button15.Enabled = true;
+            button16.Enabled = true;
+            button17.Enabled = true;
+            button18.Enabled = true;
+            button19.Enabled = true;
+            button20.Enabled = true;
+        }
+
+        private void offButton_Click(object sender, EventArgs e) //off button
+        {
+            disable();
+        }
+
+        private void onButton_Click(object sender, EventArgs e) //on button
+        {
+            enable();
+        }
+        
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+        }
+
+        private void backspace_Click(object sender, EventArgs e)
+        {
+            int lenght = textBox1.TextLength - 1;
+            string text = textBox1.Text;
+            textBox1.Clear();
+            for (int i = 0; i < lenght; i++)
+                textBox1.Text = textBox1.Text + text[i];
+        }
+
+        private void button_click(object sender, EventArgs e)
+        {
+            if ((textBox1.Text == "0") || (isOperationPerformed))
+            {
+                textBox1.Clear();
+            }
+            isOperationPerformed = false;
+            Button button = (Button)sender;
+            if (button.Text == ".")
+            {
+                if (!textBox1.Text.Contains("."))
+                    textBox1.Text = textBox1.Text + button.Text;
+            }else
+            textBox1.Text = textBox1.Text + button.Text;
+        }
+
+        private void operator_click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+
+            if (resultValue != 0)
+            {
+                button20.PerformClick();
+                operationPerformed = button.Text;
+                label1.Text = resultValue + " " + operationPerformed;
+                isOperationPerformed = true;
+            }
+            else
+            {
+                operationPerformed = button.Text;
+                resultValue = Double.Parse(textBox1.Text);
+                label1.Text = resultValue + " " + operationPerformed;
+                isOperationPerformed = true;
+            }
+        }
+
+        private void  result(object sender, EventArgs e)
+        {
+            switch (operationPerformed)
+            {
+                case "+":
+                    textBox1.Text = (resultValue + Double.Parse(textBox1.Text)).ToString();
+                    break;
+                case "-":
+                    textBox1.Text = (resultValue - Double.Parse(textBox1.Text)).ToString();
+                    break;
+                case "x":
+                    textBox1.Text = (resultValue * Double.Parse(textBox1.Text)).ToString();
+                    break;
+                case "/":
+                    textBox1.Text = (resultValue / Double.Parse(textBox1.Text)).ToString();
+                    break;
+                default:
+                    break;
+            }
+            resultValue = Double.Parse(textBox1.Text);
+            label1.Text = "";
+            isOperationPerformed = true;
+            operationPerformed = "";
+        }
+    }
+}
---
+++ Calculator (Homework 1)/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator__Homework_1_
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
